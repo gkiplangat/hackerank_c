4323139bcda5c30b721e26e3ca32adedd1d00b93
gkiplangat <gkiplangat>
2023-04-15 13:52:41 +0300

    Hello world

---
+++ hello_world.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+/**
+ * main -Entry point
+ * Return: 0 on success
+ */
+
+int main(void)
+	{
+	/* Enter your code here. Read input from STDIN. Print output to STDOUT */
+	printf("Hello, World!\n");
+	
+	return (0);
+}
